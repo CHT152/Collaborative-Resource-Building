5c8e05611c46e28bdcfa5910f1fb5ff7b8acad66
CHT152 <108377924+CHT152@users.noreply.github.com>
2022-08-24 10:53:44 +0800

    Create Word forming.py

---
+++ Word forming.py
@@ -0,0 +1,8 @@
+# You are given an array of strings words and a string chars.
+# A string is good if it can be formed by characters from chars (each character can only be used once).
+# Return the sum of lengths of all good strings in words.
+
+# Example 
+# Input: words = ["cat","bt","hat","tree"], chars = "atach"
+# Output: 6
+# Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
