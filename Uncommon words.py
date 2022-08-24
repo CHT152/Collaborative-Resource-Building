b4842b2c097e702364aa985b4a1cc3383988e948
CHT152 <108377924+CHT152@users.noreply.github.com>
2022-08-24 10:42:33 +0800

    Create Uncommon words.py

---
+++ Uncommon words.py
@@ -0,0 +1,2 @@
+# A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+# Given two sentences s1 and s2, return a list of all the uncommon words in any order.
